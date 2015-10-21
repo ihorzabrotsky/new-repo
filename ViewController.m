2052986b31d843dce21f8749164f2204ea27c686
ihorzabrotsky <ihor.zabrotsky@gmail.com>
2015-10-21 13:32:38 +0300

    Obj-C-file added

---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test_project
+//
+//  Created by Ihor Zabrotsky on 10/21/15.
+//  Copyright (c) 2015 Ihor Zabrotsky. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
